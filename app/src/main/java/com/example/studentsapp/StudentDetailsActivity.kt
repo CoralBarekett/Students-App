5b8cef460f15230a540ce88da9da600bb6fcd441
CoralBarekett <coral.bareket@cs.colman.ac.il>
2024-12-31 10:38:21 +0200

    Refactor student details activity name

--- app/src/main/java/com/example/studentsapp/StudentDetailsActivity.kt
+++ app/src/main/java/com/example/studentsapp/StudentDetailsActivity.kt
@@ -10,23 +10,23 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class StudentsDetailsActivity : AppCompatActivity() {
+class StudentDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_students_details)
+        setContentView(R.layout.activity_student_details)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val checkedButton: CheckBox = findViewById(R.id.students_details_activity_checked_button)
-        val editButton: Button = findViewById(R.id.students_details_activity_edit_button)
-        val nameEditText: EditText = findViewById(R.id.students_details_activity_name_edit_text)
-        val idEditText: EditText = findViewById(R.id.students_details_activity_id_edit_text)
-        val phoneEditText: EditText = findViewById(R.id.students_details_activity_phone_edit_text)
-        val addressEditText: EditText = findViewById(R.id.students_details_activity_address_edit_text)
+        val checkedButton: CheckBox = findViewById(R.id.student_details_activity_checked_button)
+        val editButton: Button = findViewById(R.id.student_details_activity_edit_button)
+        val nameEditText: EditText = findViewById(R.id.student_details_activity_name_edit_text)
+        val idEditText: EditText = findViewById(R.id.student_details_activity_id_edit_text)
+        val phoneEditText: EditText = findViewById(R.id.student_details_activity_phone_edit_text)
+        val addressEditText: EditText = findViewById(R.id.student_details_activity_address_edit_text)
 
         editButton.setOnClickListener {
             val name = nameEditText.text.toString().trim()
